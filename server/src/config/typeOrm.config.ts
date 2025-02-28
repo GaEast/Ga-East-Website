9a9421b0e66d8186266fb0079edfc03cfa976367
SadickYahaya <sadickashton@live.com>
2025-02-28 23:18:40 +0000

    db

--- server/src/config/typeOrm.config.ts
+++ server/src/config/typeOrm.config.ts
@@ -7,7 +7,7 @@ export default registerAs('typeOrm.config', (): TypeOrmModuleOptions => {
     return {
       type: 'mysql',
       host: process.env.DB_HOST_PROD,
-      port: Number(process.env.DB_PORT_PROD),
+      port: Number(process.env.DB_PORT_PROD) || 3306,
       username: process.env.DB_USER_PROD,
       password: process.env.DB_PASSWORD_PROD,
       database: process.env.DB_NAME_PROD,
@@ -18,7 +18,7 @@ export default registerAs('typeOrm.config', (): TypeOrmModuleOptions => {
     return {
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
+      port: Number(process.env.DB_PORT) || 3306,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
